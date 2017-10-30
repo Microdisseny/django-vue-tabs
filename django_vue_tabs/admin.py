b4e63b885bee8b187bd7768b30af357b59677f80
Manel Clos <manelclos@gmail.com>
2017-10-31 00:03:07 +0100

    use media property and extend previous js definition

--- django_vue_tabs/admin.py
+++ django_vue_tabs/admin.py
@@ -1,18 +1,21 @@
+from django import forms
 from . import settings
 
 
 class TabsMixin(object):
     change_form_template = 'django_vue_tabs/change_form.html'
 
-    class Media:
+    @property
+    def media(self):
         css = {
             'all': ('django_vue_tabs/vue-tabs-component-1.1.0.css',)
         }
-        js = []
+        js = super(TabsMixin, self).media._js
         if settings.DJANGO_VUE_TABS_USE_VUE_JS:
             js.append('django_vue_tabs/vue-2.4.2.min.js')
         js += ['django_vue_tabs/vue-tabs-component-1.1.0.js',
                'django_vue_tabs/tabs.js']
+        return forms.Media(css=css, js=js)
 
     def add_view(self, request, form_url='', extra_context=None):
         extra_context = extra_context or {}
